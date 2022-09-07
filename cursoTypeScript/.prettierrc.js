dbf9ddb3bc493b60454e0faad64acf5c1c077576
Johannes <johannes001002@gmail.com>
2022-09-07 00:13:18 -0300

    class ts

--- cursoTypeScript/.prettierrc.js
+++ cursoTypeScript/.prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   semi: true,
   singleQuote: true,
-  printWidth: 120,
+  printWidth: 80,
   tabWidth: 2,
 };
---
+++ cursoTypeScript/src/A0018-class/A0018-class.ts
@@ -0,0 +1,33 @@
+export class Empresa {
+  public readonly nome: string;
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string;
+
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  adicionaColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  mostrarColaboradores(): void {
+    for (const colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
+  }
+}
+
+class Colaborador {
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string
+  ) {}
+}
+
+const empresa1 = new Empresa('Facebook', '11.111.111/0001-11');
+const colaborador1 = new Colaborador('Johannes', 'Alves');
+empresa1.adicionaColaborador(colaborador1);
+
+empresa1.mostrarColaboradores();
